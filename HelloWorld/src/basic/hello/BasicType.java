eb25f96b278c34ca249dc42d649dd57f897277a4
landbroken <445299258@qq.com>
2019-01-26 01:13:31 +0800

    Update BasicType.java
    新增常量和类型转换

--- HelloWorld/src/basic/hello/BasicType.java
+++ HelloWorld/src/basic/hello/BasicType.java
@@ -6,9 +6,7 @@ class FreshJuice {
 }
 
 public class BasicType {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static void BasicTypePrint(){
 		//short,int,long,float,double
 		int num=1;
 		num+=2;
@@ -28,11 +26,46 @@ public class BasicType {
 		boolean isTrue=true;
 		isTrue=false;
 		System.out.println("boolean isTrue="+isTrue);
-		
+	}
+	
+	static void PrintEnumeration() {
 		//枚举
 		FreshJuice juice = new FreshJuice();
 	    juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
 	    System.out.println("枚举 juice="+juice.size);
+	}
+	
+	static void PrintConst() {
+		//在 Java 中使用 final 关键字来修饰常量，声明方式和变量类似
+		final double PI = 3.1415927;
+		System.out.println("常量final double PI = "+PI);
+	}
+	
+	static void PrintChange() {
+		//自动类型转换方向
+		//byte,short,char—> int —> long—> float —> double 
+		
+		/* 数据类型转换必须满足如下规则：
+		 * 1. 不能对boolean类型进行类型转换。
+		 * 2. 不能把对象类型转换成不相关类的对象。
+		 * 3. 在把容量大的类型转换为容量小的类型时必须使用强制类型转换。
+		 * 4. 转换过程中可能导致溢出或损失精度
+		 * 5. 浮点数到整数的转换是通过舍弃小数得到，而不是四舍五入
+		 * */
+		
+        int i1 = 123;
+        byte b = (byte)i1;//强制类型转换为byte
+        System.out.println("int强制类型转换为byte后的值等于"+b);
+	}
+	
+	public static void main(String[] args) {
+		BasicTypePrint();
+		
+		PrintEnumeration();
+		
+		PrintConst();
+		
+		PrintChange();
 	}
 
 }
